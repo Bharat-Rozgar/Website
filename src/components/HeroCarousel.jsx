89a72c4a722fe62766af1ec9f396e7ba88208d5a
Aaditya-Mittal <adty2704@gmail.com>
2025-05-15 23:02:30 +0530

    Hero Carousel images add

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -2,6 +2,9 @@ import { Carousel, ConfigProvider } from 'antd';
 import restaurantImage from '../assets/restaurant.webp';
 import luggageImage from '../assets/luggage.webp';
 import deckImage from '../assets/deck.webp';
+import cruise1Image from '../assets/cruise1.webp';
+import cruise2Image from '../assets/cruise2.webp';
+import cruise3Image from '../assets/cruise3.webp';
 import './HeroCarousel.css';
 
 const carouselThemeOverrides = {
@@ -21,10 +24,19 @@ function HeroCarousel() {
                     <img className='slide-img' src={restaurantImage} alt="Cruise worker working in the cruise Restaurant" />
                 </div>
                 <div>
-                    <img className='slide-img' src={luggageImage} alt="Cruise worker escorting the passenger to their room while helping with their luggage" />
+                    <img className='slide-img' src={luggageImage} loading='lazy' alt="Cruise worker escorting the passenger to their room while helping with their luggage" />
                 </div>
                 <div>
-                    <img className='slide-img' src={deckImage} alt="Cruise worker working on the cruise deck" />
+                    <img className='slide-img' src={deckImage} loading='lazy' alt="Cruise worker working on the cruise deck" />
+                </div>
+                <div>
+                    <img className='slide-img' src={cruise1Image} loading='lazy' alt="A cruise ship moving in water." />
+                </div>
+                <div>
+                    <img className='slide-img' src={cruise2Image} loading='lazy' alt="A cruise ship moving in water with passengers on deck." />
+                </div>
+                <div>
+                    <img className='slide-img' src={cruise3Image} loading='lazy' alt="A cruise ship moving in water in the evening." />
                 </div>
             </Carousel>
         </ConfigProvider>
